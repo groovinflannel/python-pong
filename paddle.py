5b19194667e7a47db2e09114ebd1a76410a82e12
groovinflannel <groovinflannel@gmail.com>
2023-12-11 10:54:10 -0500

    paddle resized

--- paddle.py
+++ paddle.py
@@ -7,4 +7,6 @@ class Paddle:
         turt.shape("square")
         turt.color("white")
         turt.penup()
+        turt.resizemode("user")
+        turt.turtlesize(5, 2, 0)
         turt.setposition(350, 0)
